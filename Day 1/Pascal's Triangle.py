00498f510e0e5343c93518ef6448bd74383c439a
mradul13 <39159081+mradul13@users.noreply.github.com>
2022-04-27 12:50:15 +0530

    Update Pascal's Triangle.py

--- Day 1/Pascal's Triangle.py
+++ Day 1/Pascal's Triangle.py
@@ -12,7 +12,7 @@ class Solution:
         for i in range(numRows):
             temp = [0] + res[-1] + [0]
             sum = []
-            for j in range(len(res[-1]+1):
+            for j in range(len(res[-1])+1):
                 sum.append(temp[j] + temp[j+1])
             res.append(sum)
         return res
